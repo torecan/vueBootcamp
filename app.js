9857ebeecb68e4c99a6762bd1076da4990e252d8
Torecan Celik <torecancelik@gmail.com>
2024-05-10 22:11:10 +0200

    kickoff

--- app.js
+++ app.js
@@ -2,22 +2,14 @@ const vm = Vue.createApp({
     data(){
         return {
             name: "Tore",
-            surname: "Celik"
+            surname: "Celik",
+            url: "google.com"
         }
     },
     methods: {
         fullName(){
-            return `${this.name} ${this.surname}`
+            return `${this.name} ${this.surname.toUpperCase()}`
         }
     }
 }).mount('#app')
 
-
-// Vue.createApp({
-//     data(){
-//         return {
-//             name: "Tore",
-//             surname: "Celik"
-//         }
-//     }
-// }).mount('#app2')
